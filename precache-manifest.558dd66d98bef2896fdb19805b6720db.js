1c179a30a69adbf1b5eafec4624f3eb3ce790caf
Mike Champagne <geauxdrum@gmail.com>
2020-10-20 10:38:07 -0500

    Updates

---
+++ precache-manifest.558dd66d98bef2896fdb19805b6720db.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abfd761a92b94a7b5a282f7ddc4859f9",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "908dabfcb26e83c3c761",
+    "url": "/weatherapp/static/css/main.847b26aa.chunk.css"
+  },
+  {
+    "revision": "2d07334414643735d5f6",
+    "url": "/weatherapp/static/js/2.e6abd85b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weatherapp/static/js/2.e6abd85b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "908dabfcb26e83c3c761",
+    "url": "/weatherapp/static/js/main.e662538a.chunk.js"
+  },
+  {
+    "revision": "8862584b26a768bb4b89",
+    "url": "/weatherapp/static/js/runtime-main.cb4f0870.js"
+  },
+  {
+    "revision": "3237f7da426fbedf7faa6782f8c4c455",
+    "url": "/weatherapp/static/media/wi-day-cloudy.3237f7da.svg"
+  },
+  {
+    "revision": "4b9869a6859846e524d1985de86894f7",
+    "url": "/weatherapp/static/media/wi-day-sunny.4b9869a6.svg"
+  },
+  {
+    "revision": "c356784f45aaa40ef57ed9031101d912",
+    "url": "/weatherapp/static/media/wi-fog.c356784f.svg"
+  },
+  {
+    "revision": "7c36f2b5180391599389cc38849eb89d",
+    "url": "/weatherapp/static/media/wi-night-alt-cloudy.7c36f2b5.svg"
+  },
+  {
+    "revision": "bc016c3b72a5843f21623b98e9c90f3d",
+    "url": "/weatherapp/static/media/wi-night-alt-rain.bc016c3b.svg"
+  },
+  {
+    "revision": "f86c33c4ccc36dbeb826cbdb5f50594b",
+    "url": "/weatherapp/static/media/wi-night-clear.f86c33c4.svg"
+  },
+  {
+    "revision": "e13be320202dc94ef0d3696c1f508d24",
+    "url": "/weatherapp/static/media/wi-rain.e13be320.svg"
+  }
+]);
